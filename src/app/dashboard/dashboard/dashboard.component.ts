83293978591f77132916ffccec1687a02d4dd0bc
luizmdeveloper <luizmariodev@gmail.com>
2019-10-17 17:51:11 -0300

    Adicionando protótipo do dashboard

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faCashRegister, faChartBar, faChartLine } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+
+  faCashRegister = faCashRegister;
+  faChartLine = faChartLine;
+  faChartBar = faChartBar;
 
   constructor() { }
 
