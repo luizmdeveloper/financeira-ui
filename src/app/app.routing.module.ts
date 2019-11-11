88c12b85b41f32db7db084d78d5bf3c27785266e
luizmdeveloper <luizmariodev@gmail.com>
2019-11-11 01:34:19 -0300

    Configurando rotas de paginas não encontrada e não autorizada

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'pagina-nao-encontrada',  component: PaginaNaoEncontradaComponent },
+  { path: 'nao-autorizado', component: NaoAutorizadoComponent },
+  { path: '**', redirectTo: 'nao-encontrada' }
 ];
 
 @NgModule({
